02b7aca23b437f2993ed8320f832bdde51dc324a
lizhixi <463716459@.qq.com>
2020-02-08 15:26:03 +0800

    init git-01-test

--- src/main/java/com/bjpowernode/git/HelloWorld.java
+++ src/main/java/com/bjpowernode/git/HelloWorld.java
@@ -1,6 +1,6 @@
 package com.bjpowernode.git;
 
-public class HelloWolrd {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World -- mybranchdfd");
     }
